b8fdcc7b6ce724cff5184c32c79dfa7657b81380
twanlee <levantuan6898@gmail.com>
2020-07-29 09:04:21 +0700

    add group

--- src/app/group-management/group-management.component.ts
+++ src/app/group-management/group-management.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {IGroup} from '../IGroup';
 
 @Component({
   selector: 'app-group-management',
@@ -6,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./group-management.component.css']
 })
 export class GroupManagementComponent implements OnInit {
+  groupList : IGroup[] = [
+    {
+      id:1,
+      name:'A'
+    },
+    {
+      id:2,
+      name:'B'
+    }, {
+      id:3,
+      name:'C'
+    }
+  ];
 
   constructor() { }
 
